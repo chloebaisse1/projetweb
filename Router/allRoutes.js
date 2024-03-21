fe8010ef55d0e14a38208c34450198bc9a24b148
nom <baissechloe@hmail.com>
2024-03-21 17:01:44 +0100

    page inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,7 @@ import Route from "./Route.js"
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
-  new Route("/signin", "connexion", "/pages/signin.html"),
+  new Route("/signin", "Connexion", "/pages/signin.html"),
+  new Route("/signup", "Inscription", "/pages/signup.html"),
 ]
 export const websiteName = "Quai Antique"
