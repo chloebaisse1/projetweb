2a3ef3a7a595a8f9608a79441c9b850919827514
nom <baissechloe@hmail.com>
2024-03-21 17:28:42 +0100

    ajout pages account, edithpassword

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js"
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
-  new Route("/signin", "Connexion", "/pages/signin.html"),
-  new Route("/signup", "Inscription", "/pages/signup.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
+  new Route(
+    "/edithPassword",
+    "changement de mot de passe",
+    "/pages/auth/editPassword.html"
+  ),
 ]
 export const websiteName = "Quai Antique"
