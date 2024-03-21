d1e7102dc88430f8ee5fc3e771a0d2e3f33349db
nom <baissechloe@hmail.com>
2024-03-21 16:50:57 +0100

    page connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,5 +2,6 @@ import Route from "./Route.js"
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
+  new Route("/signin", "connexion", "/pages/signin.html"),
 ]
 export const websiteName = "Quai Antique"
